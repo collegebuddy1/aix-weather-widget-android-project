f4ef677d2fee74f130369142cd85647e4bfcb680
ritik-prog <ritikmakhija2002@gmail.com>
2024-01-13 12:09:45 +0530

    Initial commit

---
+++ Aix/app/src/main/java/net/veierland/aix/widget/AixWidgetDrawException.java
@@ -0,0 +1,10 @@
+package net.veierland.aix.widget;
+
+@SuppressWarnings("serial")
+public class AixWidgetDrawException extends Exception {
+	
+	public AixWidgetDrawException(String message) {
+		super(message);
+	}
+	
+}
